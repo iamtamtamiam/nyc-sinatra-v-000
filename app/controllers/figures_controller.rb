c59986f05b984141342f6166fe5e67b32e579e1d
Tam Vu <vutam_a@hotmail.com>
2019-12-24 17:23:00 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -29,6 +29,7 @@ class FiguresController < ApplicationController
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
     @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/edit'
   end
 
